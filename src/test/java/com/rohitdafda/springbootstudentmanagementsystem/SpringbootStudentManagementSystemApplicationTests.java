81897dde191c34cb3129f6144516dc78c60de857
rohit4netsol <rohit4netsol@gmail.com>
2025-04-28 03:47:06 +0530

    initial commit
    Signed-off-by: rohit4netsol <rohit4netsol@gmail.com>

---
+++ src/test/java/com/rohitdafda/springbootstudentmanagementsystem/SpringbootStudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohitdafda.springbootstudentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudentManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
